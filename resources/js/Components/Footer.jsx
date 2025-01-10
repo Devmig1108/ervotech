7ba9a141e23dee00f3f4a9d492322bb178226eff
devmig1108 <devmig1108@gmail.com>
2025-01-10 13:06:08 -0700

    footer dar/light logo

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -27,7 +27,10 @@ export default function Footer() {
 
                 {/* Call-to-Action Section */}
                 <div className="footer-cta">
-                    <img src="images/ervotech-logo.png" alt="Company Logo" className="cta-logo" />
+                    <Link href="/">
+                        <img className="light-logo" src="images/ervotech-logo.png" alt="Logo" />
+                        <img className="dark-logo" src="images/ervotech-logo-dark.png" alt="Logo" />
+                    </Link>
                     {/* <h4>Let's Build Something Extraordinary</h4> */}
                     <p>Ready to take your digital presence to the next level? Contact us today and let's make it happen!</p>
                     <a href="/contact" className="cta-button">Get in Touch</a>
