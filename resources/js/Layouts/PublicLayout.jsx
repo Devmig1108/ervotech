c6640333878c44d06bcad6fa431b71130dc891fe
miguel112107 <maflores1108@gmail.com>
2025-05-16 17:04:05 -0600

    remove padding from site on small screens

--- resources/js/Layouts/PublicLayout.jsx
+++ resources/js/Layouts/PublicLayout.jsx
@@ -15,7 +15,7 @@ export default function Public({ children }) {
         <>
             <Nav />
             <ParallaxProvider>
-                <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <div className="sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                     {/* Page Content */}
                     {children}
                 </div>
