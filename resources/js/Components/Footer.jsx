c0919d84dc411de262aaffb039f3c336beee5b32
Miguel <devmig1108@gmail.com>
2026-02-19 11:00:03 -0700

    update copyright date

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -37,7 +37,7 @@ export default function Footer() {
                 </div>
             </div>
             <div className="footer-bottom">
-                <p>&copy; 2025 Ervotech. All Rights Reserved.</p>
+                <p>&copy; 2026 Ervotech. All Rights Reserved.</p>
             </div>
         </footer>
 
