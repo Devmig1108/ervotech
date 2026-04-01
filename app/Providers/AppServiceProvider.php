29ded5088d468445f1834f936345a3beee49c5e3
Miguel <devmig1108@gmail.com>
2026-04-01 01:21:18 -0600

    new email config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,19 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Mail;
-// Update this line specifically:
-use ZeptoMailTransactional\Transport\ZeptoMailTransport;
-use ZeptoMailTransactional\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
-    // ...
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        Mail::extend('zeptomail', function () {
-            // This now correctly references the class above
-            return new ZeptoMailTransport(new ApiClient());
-        });
+        //
     }
 }
