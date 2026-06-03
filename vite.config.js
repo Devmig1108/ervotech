eddfb90a920f56ab232eb2d291b2606597098ecd
Miguel <devmig1108@gmail.com>
2026-06-03 02:12:57 -0600

    fix: inline react-helmet-async for ssr bundle compilation

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,15 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', // Or your global CSS file path
+                'resources/css/app.css', 
                 'resources/js/app.jsx',
             ],
-            ssr: 'resources/js/ssr.jsx', // <-- ADD THIS LINE
+            ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
         react(),
     ],
+    ssr: {
+        noExternal: ['react-helmet-async'],
+    },
 });
